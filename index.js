0750464cc714af0004f24e5b1d83635a1315b60c
SemVR <sem.wlad26@gmail.com>
2025-04-10 17:05:35 +0300

    fix3

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 //#1 getRandomInt
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min);
 }
 console.log(getRandomInt(1, 10));
